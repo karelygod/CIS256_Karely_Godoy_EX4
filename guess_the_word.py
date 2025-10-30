3fb90132cec0c7bcea9b04d7947811bce9246500
Karely Godoy <kar.god21@gmail.com>
2025-10-30 12:07:15 -0700

    Added game logic and tests

---
+++ guess_the_word.py
@@ -0,0 +1,44 @@
+import random
+""" Creates an interactive guessing game
+User guesses the word with limited guesses.
+"""
+# Create list of randoms words. 
+random_word = ["panda", "flowers", "roses", "road", "plane"]
+# A random word is selected ans stored.
+word_choice = random.choice(random_word)
+
+# Set the limit number of guessses to 5.
+limit = 7
+# Define empty string that stores attempts.
+guesses = ''
+# Loop runs as long as user has guesses left.
+while limit > 0:
+    # Counts characters guessed incorrectly so far.
+    failed = 0
+    # If guess is correct display the word. If not display underscore. 
+    for w in word_choice:
+        if w in guesses:
+            print(w, end=" ")
+        else:
+            print("_", end=" ")
+            failed += 1 # Store the incorrect guess.
+    print()
+    # If all characters are guessed. Display the word.
+    if failed == 0:
+        print(f"Congratulations! You guessed the word {word_choice}.")
+        break
+    else:
+        print()
+    # Prompt user to guess a letter.
+    word_guessed = input("Guess a letter: ")
+    # Store attempt in string.
+    guesses += word_guessed
+    # If the word is guessed incorrectly, store attempt.
+    if word_guessed not in word_choice:
+        limit -= 1
+        print(f"Incorrect word. You have {limit} guesses left")
+        # Once user runs out of limits, game over.
+        if limit == 0:
+            print("Game over, no more guesses left.")
+
+
---
+++ test_guess_the_word.py
@@ -0,0 +1,57 @@
+import random
+import unittest
+import importlib
+import io
+import builtins
+import sys
+from unittest import mock
+import guess_the_word as game
+
+class TestGuess(unittest.TestCase):
+    # Test random module is picking from predefined list.
+    def test_if_in_predefined_list(self):
+        with mock.patch.object(random, "choice", lambda seq: seq[0]):
+            # reload to run with "choice"
+            importlib.reload(game)
+        self.assertTrue(game.word_choice in game.random_word)
+
+    # Test correct guesses diplay message.
+    def test_correct_guesses(self):
+        importlib.reload(game)
+        game.word_choice = "roses"
+        game.limit = 7
+        game.guesses = []
+
+        # Input what should be in list.
+        input = ["r", "o", "s", "e", "s"]
+        with mock.patch.object(builtins, "input", side_effect=input):
+            x = io.StringIO()
+            with mock.patch.object(sys, "stdout", x):
+                game.guess_word()
+        o = x.getvalue()
+
+        self.assertTrue("Congratulations" in o)
+        self.assertTrue("roses" in o)
+
+    # Testing if exhausting wrong guesses will limit.
+    def test_wrong_guesses(self):
+        importlib.reload(game)
+        game.word_choice = "road"
+        game.limit = 7
+        game.guesses = []
+
+        # Test system is identifying wrong words.
+        wrong_word = ["x", "y", "z", "q", "t"]
+        with mock.patch.object(builtins, "input", side_effect=wrong_word):
+            x = io.StringIO()
+            with mock.patch.object(sys, "stdout", x):
+                game.guess_word()
+        o = x.getvalue()
+
+        # Ensure messages are correct.
+        self.assertTrue("Incorrect" in o)
+        self.assertTrue("Game over" in o)
+
+
+if __name__ == "__main__":
+    unittest.main()
